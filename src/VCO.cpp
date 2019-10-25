8d15600396e5e1c74c54441b73de951046338945
Thanh Do <thanh.do@digia.com>
2019-10-25 10:37:24 +0300

    add panel for first module VCO
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- src/VCO.cpp
+++ src/VCO.cpp
@@ -3,54 +3,40 @@
 
 struct VCO : Module {
 	enum ParamIds {
-		PITCH_PARAM,
+		FREQ_PARAM,
+		FM_PARAM,
+		PWM_PARAM,
+		PWIDTH_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		PITCH_INPUT,
+		FM_INPUT,
+		SYNC_INPUT,
+		PWM_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
-		SINE_OUTPUT,
+		SIN_OUTPUT,
+		TRI_OUTPUT,
+		SAW_OUTPUT,
+		SQR_OUTPUT,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
-		BLINK_LIGHT,
+		PATH5729_1_LIGHT,
 		NUM_LIGHTS
 	};
 
-	float phase = 0.f;
-    float blinkPhase = 0.f;
-
 	VCO() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(FREQ_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(FM_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(PWM_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(PWIDTH_PARAM, 0.f, 1.f, 0.f, "");
 	}
 
 	void process(const ProcessArgs& args) override {
-		// Compute the frequency from the pitch parameter and input
-        float pitch = params[PITCH_PARAM].getValue();
-        pitch += inputs[PITCH_INPUT].getVoltage();
-        pitch = clamp(pitch, -4.f, 4.f);
-        // The default pitch is C4 = 261.6256f
-        float freq = dsp::FREQ_C4 * std::pow(2.f, pitch);
-
-        // Accumulate the phase
-        phase += freq * args.sampleTime;
-        if (phase >= 0.5f)
-            phase -= 1.f;
-
-        // Compute the sine output
-        float sine = std::sin(2.f * M_PI * phase);
-        // Audio signals are typically +/-5V
-        // https://vcvrack.com/manual/VoltageStandards.html
-        outputs[SINE_OUTPUT].setVoltage(5.f * sine);
-
-        // Blink light at 1Hz
-        blinkPhase += args.sampleTime;
-        if (blinkPhase >= 1.f)
-            blinkPhase -= 1.f;
-        lights[BLINK_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
 	}
 };
 
@@ -65,13 +51,25 @@ struct VCOWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, VCO::PITCH_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.0, 29.5)), module, VCO::FREQ_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(36.5, 44.5)), module, VCO::FM_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(36.5, 60.5)), module, VCO::PWM_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(36.5, 76.5)), module, VCO::PWIDTH_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, VCO::PITCH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8.0, 98.5)), module, VCO::PITCH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(19.8, 98.5)), module, VCO::FM_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(31.6, 98.5)), module, VCO::SYNC_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(43.4, 98.5)), module, VCO::PWM_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, VCO::SINE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.0, 116.5)), module, VCO::SIN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.8, 116.5)), module, VCO::TRI_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(31.6, 116.5)), module, VCO::SAW_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(43.4, 116.5)), module, VCO::SQR_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, VCO::BLINK_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(22.25, 18.25)), module, VCO::PATH5729_1_LIGHT));
+
+		// mm2px(Vec(4.0, 7.0))
+		addChild(createWidget<Widget>(mm2px(Vec(34.5, 21.5))));
 	}
 };
 
