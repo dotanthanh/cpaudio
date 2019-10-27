fef0ffcb7548305400bad3b48d13859332b16adb
Thanh Do <thanh.do@digia.com>
2019-10-27 15:57:49 +0200

    enable modulation for FM, pitch, pulse width, add sync options
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- src/VCO.cpp
+++ src/VCO.cpp
@@ -32,32 +32,76 @@ struct VCO : Module {
 	// default frequency A4 (tuning standard)
 	float baseFreq = dsp::FREQ_A4;
 	float phase = 0.f;
+	float lastSyncInput = 0.f;
+	// for soft sync, reverse sync direction whenever sync input finishes a cycle
+	float syncDirection = 1.f;
 
 	VCO() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		// A1 -> A7
-		configParam(FREQ_PARAM, -28.9f, 28.9f, 0.f, "Frequency", " Hz", dsp::FREQ_SEMITONE, dsp::FREQ_A4);
-		configParam(FM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(PWM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(PWIDTH_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(SYNC_PARAM, 0.f, 1.f, 0.f, "");
+		// A0 -> A8
+		configParam(FREQ_PARAM, -48.f, 48.f, 0.f, "Frequency", " Hz", dsp::FREQ_SEMITONE, dsp::FREQ_A4);
+		configParam(FM_PARAM, 0.f, 1.f, 0.f, "FM", "%", 0.f, 100.f);
+		configParam(PWM_PARAM, 0.f, 1.f, 0.f, "Pulse width modulation", "%", 0.f, 100.f);
+		configParam(PWIDTH_PARAM, 0.f, 1.f, 0.5f, "Pulse width", "%", 0.f, 100.f);
+		configParam(SYNC_PARAM, 0.f, 1.f, 1.f, "Sync mode");
 	}
 
 	void process(const ProcessArgs& args) override {
-		float pitch = params[FREQ_PARAM].getValue() / 12.f;
-        // pitch += inputs[PITCH_INPUT].getVoltaFREQ;
-        float freq = baseFreq * std::pow(2.f, pitch);
+		float freqParam = params[FREQ_PARAM].getValue() / 12.f;
+		float fmParam = params[FM_PARAM].getValue();
+		float pwmParam = params[PWM_PARAM].getValue();
+		float pwidthParam = params[PWIDTH_PARAM].getValue();
+		bool isHardSync = params[SYNC_PARAM].getValue() > 0;
 
-        // Accumulate the phase
-        phase += freq * args.sampleTime;
+		float pitch = freqParam;
+
+		if (inputs[PITCH_INPUT].isConnected()) {
+			pitch += inputs[PITCH_INPUT].getVoltage();
+		}
+
+		if (inputs[FM_INPUT].isConnected()) {
+			pitch += fmParam * inputs[FM_INPUT].getVoltage();
+		}
+
+		pitch = clamp(pitch, -4.f, 4.f);
+        float freq = baseFreq * std::pow(2.f, pitch);
+		// Accumulate the phase (or reset partway of sync direction is reversed)
+        phase += freq * args.sampleTime * syncDirection;
 		// wrap phase in range [-0.5, 0.5]
-		phase -= phase > 0.5f ? 1.f : 0.f;
+		phase -= phase >= 0.5f || phase <= -0.5f ? std::trunc(phase) : 0.f;
+
+		if (inputs[SYNC_INPUT].isConnected()) {
+			float syncInput = inputs[SYNC_INPUT].getVoltage();
+			if (lastSyncInput <= 0 && syncInput >= 0) {
+				if (isHardSync) {
+					phase = 0.f;
+				} else {
+					// reverse sync direction
+					syncDirection *= -1.f; 
+				}
+			}
+			lastSyncInput = syncInput;
+		}
 
         // Audio signals are typically +/-5V
-        outputs[SIN_OUTPUT].setVoltage(5.f * sin(phase));
-        outputs[TRI_OUTPUT].setVoltage(5.f * tri(phase));
-		outputs[SAW_OUTPUT].setVoltage(5.f * saw(phase));
-		outputs[SQR_OUTPUT].setVoltage(5.f * sqr(phase));
+		if (outputs[SIN_OUTPUT].isConnected()) {
+        	outputs[SIN_OUTPUT].setVoltage(5.f * sin(phase));
+		}
+		if (outputs[TRI_OUTPUT].isConnected()) {
+  	    	outputs[TRI_OUTPUT].setVoltage(5.f * tri(phase));
+		}
+		if (outputs[SAW_OUTPUT].isConnected()) {
+			outputs[SAW_OUTPUT].setVoltage(5.f * saw(phase));
+		}
+		if (outputs[SQR_OUTPUT].isConnected()) {
+			float pulseWidth = pwidthParam;
+			if (inputs[PWM_INPUT].isConnected()) {
+				pulseWidth += pwmParam * inputs[PWM_INPUT].getVoltage() / 10.f;
+			}
+			// avoid DC
+			pulseWidth = clamp(pulseWidth, 0.01f, 0.99f);
+        	outputs[SQR_OUTPUT].setVoltage(5.f * sqr(phase, pulseWidth));
+		}
 	}
 
 	// need alias reduction for all these waves here
@@ -75,8 +119,8 @@ struct VCO : Module {
 		return 2.f * phase;
 	}
 
-	float sqr(float phase) {
-		return phase > 0 ? 1.f : -1.f;
+	float sqr(float phase, float pulseWidth) {
+		return phase > (pulseWidth * 1.f - 0.5f) ? -1.f : 1.f;
 	}
 };
 
